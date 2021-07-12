a1029ec266429040f77896740910b8758be79eb9
Amith <amithsai78@gmail.com>
2021-07-12 06:15:10 +0530

    Added Routing using React Router Dom

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 import './App.css';
 import Header from './components/header/Header.component';
+import Home from './page/home/Home.component';
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <Route exact path='/' component={Home}></Route>
     </div>
   );
 }
