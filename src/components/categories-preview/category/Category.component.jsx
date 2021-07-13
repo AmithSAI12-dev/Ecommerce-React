bcab6203496aaa2d7714cf29c78cf585d9443c7a
Amith <amithsai78@gmail.com>
2021-07-13 14:15:31 +0530

    Update Category Component

--- src/components/categories-preview/category/Category.component.jsx
+++ src/components/categories-preview/category/Category.component.jsx
@@ -2,20 +2,20 @@ import React from 'react'
 import './Category.style.css';
 import CustomBtn from '../../custom-btn/CustomBtn.component';
 
-function Category() {
+function Category({name, image}) {
     return (
         <a href="/" className='category'>
             {/* Category Image */}
             <div className="category__img">
-                <img src="https://cdn.shopify.com/s/files/1/0274/4293/7933/files/Skateboardbanners__0002_NK7B3073_x1200.jpg?v=1625759575" alt="Category Name" />
+                <img src={image} alt={name} />
             </div>
             {/* Category Title */}
             <div className="category__titleWrapper">
-                <h4 className="category__title">Completes</h4>
+                <h4 className="category__title">{name}</h4>
             </div>
             {/* Category Title Hover Only */}
             <div className="category__titleWrapper hover-only">
-                <div className="category__title">Completes</div>
+                <div className="category__title">{name}</div>
             </div>
             {/* Custom Button Component */}
             <div className="category__btn">
