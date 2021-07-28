07f30a6451af6e450ac230d17d82d815a8fe5f52
Amith Sai <amithsai78@gmai.com>
2021-07-28 06:20:58 +0530

    Updated Product Preview Component

--- src/components/product-preview/ProductPreview.component.jsx
+++ src/components/product-preview/ProductPreview.component.jsx
@@ -3,9 +3,9 @@ import Product from './product/Product.component'
 import './ProductPreview.style.css';
 import { products } from '../product-preview/product/product.data';
 
-function ProductPreview() {
+function ProductPreview({columns}) {
     return (
-        <div className='productPreview mt-50'>
+        <div className={`productPreview mt-50 ${columns===4 ? 'col-4' : 'col-6'}`}>
             {/* Products */}
             {
                 products.map(props => (
